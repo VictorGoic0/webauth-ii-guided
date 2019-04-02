33aaf764998209969e0c432a166c9f8ec9e1a8f2
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-02 14:01:17 -0400

    Guided demo of cookies / sessions

--- api/server.js
+++ api/server.js
@@ -1,20 +1,43 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const knexConfig = require("../database/dbConfig.js");
+const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it salty",
+  cookie: {
+    maxAge: null,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: knexConfig,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30 // Delete expired sessions
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
